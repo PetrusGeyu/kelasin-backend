bca6ba5c06e481b3d6efa4bdaf9a3e50d8460d61
Marvello Nyahu <91764649+Marvvllo@users.noreply.github.com>
2025-10-05 18:23:48 +0800

    Added gemfile rack-cors, edited cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,18 @@
-# Be sure to restart your server when you modify this file.
+# config/initializers/cors.rb
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # Allow requests from your client running on http://localhost:3000
+  allow do
+    origins 'http://localhost:4000' # 👈 Set this to your frontend's address
 
-# Read more: https://github.com/cyu/rack-cors
+    resource '*', # You can narrow this down (e.g., to '/api/*')
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head] # Allow all necessary methods
+  end
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+  # 💡 Optional: If you deploy your app later, you can add the production origin
+  # allow do
+  #   origins 'https://www.yourproductionapp.com'
+  #   resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  # end
+end
