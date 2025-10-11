befc0a0881c5ab03b26c6c1823e7b397718949d4
nyaivu <marvtfs@gmail.com>
2025-10-11 20:22:36 +0800

    Added New Domain to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   # Allow requests from your client running on http://localhost:3000
   allow do
-    origins 'https://kelasin.nyaivu.com' # 👈 Set this to your frontend's address
+    origins 'https://zerona.nyaivu.com' # 👈 Set this to your frontend's address
 
     resource '*', # You can narrow this down (e.g., to '/api/*')
       headers: :any,
